a499668c1a6a836b63f22265abaaecccadaedd07
Alvaro Hurtado <alvarohurtado84@gmail.com>
2015-04-17 20:15:07 +0400

    Add base structure. Just nuser.User and poster.Post models.

---
+++ nuser/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """Customized user for Notenote."""
+
+    pass
---
+++ poster/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from nuser.models import User
+
+
+class Post(models.Model):
+    """Stores every article that will be written by the users."""
+
+    content = models.TextField()
+
+    written_by = models.ForeignKey(User)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField()
+
+
