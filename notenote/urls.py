41ab17bd36f5c4412b8ed9178599514fc0d60a2e
Alvaro Hurtado <alvarohurtado84@gmail.com>
2015-04-18 18:32:05 +0400

    [home_view] Add Login and Logout API by Django Rest Framework to urls. And delegate the rest to poster.urls for now.

--- notenote/urls.py
+++ notenote/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'notenote.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+
+    # Login & Logout API by Django Rest Framework
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+
+    # Django Admin Panel urls
+    url(r'^admin/',
+        include(admin.site.urls)),
+
+    # Rest of urls for Notenote
+    url(r'^', include('poster.urls')),
+
 ]
