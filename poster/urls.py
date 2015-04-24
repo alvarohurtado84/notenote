3717f147249890d204b09dde963305e8063125a8
Alvaro Hurtado <alvarohurtado84@gmail.com>
2015-04-24 22:24:47 +0400

    [home_view] Clean poster.urls until necessary

--- poster/urls.py
+++ poster/urls.py
@@ -1,12 +1,6 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'notenote.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include('poster.urls')),
-
-    url(r'^admin/', include(admin.site.urls))
 ]
