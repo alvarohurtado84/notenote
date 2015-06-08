be3ae8e2d864d47c63a227ecdbfb1e5103445ba4
Alvaro Hurtado <alvarohurtado84@gmail.com>
2015-06-08 16:59:24 +0400

    [add_zenpen_style] Add an StartEdit method to Post, when click on Post, the object become editable.

--- static/js/app/components/Post.js
+++ static/js/app/components/Post.js
@@ -3,14 +3,35 @@ var React = require('react');
 
 
 var Post = React.createClass({
+
+    getInitialState: function() {
+        return {};
+    },
+
+    startEdit: function() {
+        this.setState({
+            editMode: true
+        });
+    },
+
+    getEditMode: function(){
+        if(this.state.editMode){
+            return this.state.editMode;
+        }else{
+            return false
+        }
+    },
+
     render: function() {
         return (
-            <article>
-                {this.props.children}
+            <div>
+                <article onClick={this.startEdit} contentEditable={this.getEditMode()} ref="myContent">
+                    {this.props.children}
+                </article>
                 <span class='author'>
-                    by {this.props.written_by}
+                    by <strong>{this.props.written_by}</strong>
                 </span>
-            </article>
+            </div>
         );
     }
 });
